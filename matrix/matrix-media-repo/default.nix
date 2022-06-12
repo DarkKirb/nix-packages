627cc1411bab7a0bcb01d74102e1d0f293c9d416
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-12 15:10:57 +0100

    fix the way thne version is calculated in the build script,

--- matrix/matrix-media-repo/default.nix
+++ matrix/matrix-media-repo/default.nix
@@ -18,7 +18,7 @@ in {
     buildPhase = ''
       GOBIN=$PWD/bin go install -v ./cmd/compile_assets
       $PWD/bin/compile_assets
-      GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.GitCommit=$(git rev-list -1 HEAD) -X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/...
+      GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.GitCommit=$(git rev-list -1 HEAD) -X github.com/turt2live/matrix-media-repo/common/version.Version=${version}" -v ./cmd/...
     '';
     installPhase = ''
       mkdir $out
