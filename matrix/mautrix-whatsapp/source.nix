b24d37bf5858866832141b66984ad163cb514fbe
DarkKirb <DarkKirb@users.noreply.github.com>
2022-06-13 05:53:33 +0000

    Automatic Update

--- matrix/mautrix-whatsapp/source.nix
+++ matrix/mautrix-whatsapp/source.nix
@@ -1,5 +1,5 @@
 {fetchgit}:
-fetchgit { 
+fetchgit {
   url = "https://github.com/mautrix/whatsapp";
   rev = "0796d14133c19d5e18223b6f138835229b7acf10";
   sha256 = "1a8d3rn9xsmxc8sd4iwfsrgia49nygc9va2n8v1ja2smn560pyq1";
