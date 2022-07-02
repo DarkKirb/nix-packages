cc1743e6adf06a76b4f99c3ddc3aa7c7d53761b7
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-02 17:51:35 +0000

    Update packages

--- matrix/mautrix-whatsapp/source.nix
+++ matrix/mautrix-whatsapp/source.nix
@@ -1,7 +1,7 @@
 {fetchgit}:
 fetchgit {
   url = "https://github.com/mautrix/whatsapp";
-  rev = "7247e9b2e6d70a71505ac3d160dc8ae3f4e3fe25";
-  sha256 = "1bygawxq3ccljx076fm1zva9ch5bmwdwfmghynbdvvgpgii5nd6d";
+  rev = "9a6670eb93538cbc82dff878659ae2b33711bc9d";
+  sha256 = "14xa6kv0y6mlyx20fgli03i02vxghywhp0v7ffvqanzhzbnqxfa6";
   leaveDotGit = true;
 }
