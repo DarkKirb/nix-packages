aa04377e5150fa663303893a585cbe43aced1727
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-06 13:40:56 +0100

    disable setuptools checkphase for ruamel.yaml.clib

--- misc/plover.nix
+++ misc/plover.nix
@@ -66,6 +66,7 @@ in rec {
     buildPythonPackage rec {
       inherit (tarballs.ruamel-yaml-clib-src.passthru) pname version;
       src = tarballs.ruamel-yaml-clib-src;
+      setuptoolsCheckPhase " true";
     };
   regenpfeifer-env = pypy3.withPackages (ps: [ps.marisa-trie]);
   wortformliste = pkgs.stdenvNoCC.mkDerivation {
