8a2029a792d61d8d6e87b1917f3903e6087edeaa
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-08 11:46:38 +0100

    switch to nixpkgs master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Lotte’s nix packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
 
