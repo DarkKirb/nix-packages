c22f9ca244f9a60f7c9504fdea19836c7bba6d29
drone <lotte@chir.rs>
2022-07-17 10:46:17 +0000

    Update packages

--- art/source.nix
+++ art/source.nix
@@ -1,7 +1,7 @@
 {fetchgit}:
 fetchgit {
   url = "https://git.chir.rs/darkkirb/lotte-art";
-  rev = "3310f3db58562548ab756371537c289287680c2e";
-  sha256 = "1wqzaiwnzxm2scfdzpmp3wwdsxlfyiy7p5b88iii2s32h57hdslk";
+  rev = "2d4e086bdb8d20661b2e8ffa20299113af67a14d";
+  sha256 = "1z62qwb1bhcm3imnbs3lkrdijz8q0yc0n6sdhg8s500442bdxrx8";
   fetchLFS = true;
 }
--- matrix/mautrix-whatsapp/source.nix
+++ matrix/mautrix-whatsapp/source.nix
@@ -1,7 +1,7 @@
 {fetchgit}:
 fetchgit {
   url = "https://github.com/mautrix/whatsapp";
-  rev = "6f5070b2435f03c8685291e6e92703b2a539956f";
-  sha256 = "1hfda92i4k62djs749y9j46vrhs4x6av108b4qacw0cic5xd7na5";
+  rev = "45ed657ab1ea01f8028c0842d51d1a9da8213aca";
+  sha256 = "0si3bxxyhkxgcvfav7sqvl82c0dl8xgpwiqrbk2glxdyrsj5blc8";
   leaveDotGit = true;
 }
