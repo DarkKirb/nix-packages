91f012070706b92b7e8a30390c82a01622d27757
drone <lotte@chir.rs>
2022-07-21 08:38:31 +0000

    Update packages

--- matrix/mautrix-discord/source.nix
+++ matrix/mautrix-discord/source.nix
@@ -1,7 +1,7 @@
 {fetchgit}:
 fetchgit {
   url = "https://github.com/mautrix/discord";
-  rev = "11efa4996186656dee186ba519d1f6893873ab5f";
-  sha256 = "0fpddiarixppdqdi1is84lag96hr7lppqvjf0v4s1jy8ljj48lbl";
+  rev = "a9b9ca72d77bc1f3ec32119cb135d36b297e60a2";
+  sha256 = "13p6b3xkd6857lh67km0hm518z7d4h3j94fs25wzpgi0lyqrwnaj";
   leaveDotGit = true;
 }
