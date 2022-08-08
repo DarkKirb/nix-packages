70add06d352d27200e87b8632ec17bafcce5a6a2
drone <lotte@chir.rs>
2022-08-08 01:28:28 +0000

    Update packages

--- matrix/mautrix-whatsapp/source.nix
+++ matrix/mautrix-whatsapp/source.nix
@@ -1,7 +1,7 @@
 {fetchgit}:
 fetchgit {
   url = "https://github.com/mautrix/whatsapp";
-  rev = "5ba69f7e8e574edeb8127e258962046756def4d8";
-  sha256 = "1m09f30awkccpbjwnlrgz4h3q8yrjcj37l3kmxpj812ya6l3pasr";
+  rev = "b7514fdc8d7da1772ae9e405777d2266832142c1";
+  sha256 = "12yz95qjw33mk95jdb4gf85x7zw8kzd5n9wy8v7swnh4mb36kjbj";
   leaveDotGit = true;
 }
