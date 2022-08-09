3920fadc24af61decb3a07511c8eb3e206b7db8f
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-08-09 08:54:33 +0100

    fix mautrix-signal

--- matrix/mautrix-signal.nix
+++ matrix/mautrix-signal.nix
@@ -28,5 +28,17 @@ in {
         yarl
       ];
       doCheck = false;
+      postInstall = ''
+        mkdir -p $out/bin
+        # Make a little wrapper for running mautrix-signal with its dependencies
+          echo "$mautrixSignalScript" > $out/bin/mautrix-signal
+          echo "#!/bin/sh
+          exec python -m mautrix_signal \"\$@\"
+          " > $out/bin/mautrix-signal
+          chmod +x $out/bin/mautrix-signal
+          wrapProgram $out/bin/mautrix-signal \
+          --set PATH ${python3}/bin \
+          --set PYTHONPATH "$PYTHONPATH"
+      '';
     };
 }
