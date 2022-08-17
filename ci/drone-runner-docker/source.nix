5ca5c15aca4e322f73ec35be47d5ba4e94e6e4d7
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-08-17 10:50:29 +0100

    bruh

--- ci/drone-runner-docker/source.nix
+++ ci/drone-runner-docker/source.nix
@@ -2,6 +2,6 @@
 fetchgit {
   url = "https://github.com/drone-runners/drone-runner-docker";
   rev = "47ee73d829ac4796415b8ccb6c96bbbf43da8d18";
-  sha256 = "0c6qjgh4iwchajacd97gv5k9ary3hhl98y5jw23nxdv6dv33lzhl";
+  sha256 = "sha256-NOQ+5orYk4GxCPBqjrJs390F/ukqqtLa39B4hF2SdPg=";
   leaveDotGit = true;
 }
