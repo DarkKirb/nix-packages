b88153f32012f67ef0ba4a30a2ddc0d2a8f58e7f
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-09-25 18:23:12 +0100

    forgot a KreativeSquare

--- fonts/kreative/kreative-square.nix
+++ fonts/kreative/kreative-square.nix
@@ -1,11 +1,6 @@
-{
-  stdenv,
-  callPackage,
-  lib,
-}: let
-  source = builtins.fromJSON (builtins.readFile ./source.json);
-in
-  stdenv.mkDerivation {
+{ stdenv, callPackage, lib }: 
+  let source = builtins.fromJSON (builtins.readFile ./source.json);
+  in stdenv.mkDerivation {
     name = "kreative-square";
     version = source.date;
     src = callPackage ./source.nix {};
@@ -13,7 +8,7 @@ in
     buildPhase = "true";
     installPhase = ''
       for variant in "" SM; do
-        install -m444 -Dt $out/share/truetype/KreativeSquare KreativeSquare/$variant.ttf
+        install -m444 -Dt $out/share/truetype/KreativeSquare KreativeSquare/KreativeSquare$variant.ttf
       done
     '';
     meta = {
