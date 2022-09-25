0094b20a4d4296d559628ae73504b24773527890
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-09-25 15:50:09 +0100

    use overrideAttrs instead of a new derivation

--- art/lotte/default.nix
+++ art/lotte/default.nix
@@ -1,22 +1,24 @@
-{ stdenv, fetchgit, lib }: let
-  src = builtins.fromJSON (builtins.readFile ./source.json);
-in stdenv.mkDerivation {
-  pname = "lotte-art";
-  version = src.date;
+{
+  stdenv,
+  fetchgit,
+  lib,
+}: let
+  srcInfo = builtins.fromJSON (builtins.readFile ./source.json);
   src = fetchgit {
-    inherit (src) url rev sha256 fetchLFS fetchSubmodules deepClone leaveDotGit;
+    inherit (srcInfo) url rev sha256 fetchLFS fetchSubmodules deepClone leaveDotGit;
   };
-  preferLocalBuild = true;
-  passthru.updateScript = [
-    ../../scripts/update-git.sh
-    "https://git.chir.rs/darkkirb/lotte-art"
-    "art/lotte/source.json"
-    "--fetch-lfs"
-  ];
-  meta = {
-    description = "Art I commissioned (mostly)";
-    license = lib.licenses.cc-by-nc-sa-40;
-  };
-  buildPhase = "true";
-  installPhase = "cp -r $src $out";
-}
+in
+  src.overrideAttrs (_: {
+    pname = "lotte-art";
+    version = srcInfo.date;
+    passthru.updateScript = [
+      ../../scripts/update-git.sh
+      "https://git.chir.rs/darkkirb/lotte-art"
+      "art/lotte/source.json"
+      "--fetch-lfs"
+    ];
+    meta = {
+      description = "Art I commissioned (mostly)";
+      license = lib.licenses.cc-by-nc-sa-40;
+    };
+  })
