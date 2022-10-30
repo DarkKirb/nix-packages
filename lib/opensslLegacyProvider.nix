273bdf17b9f5fa27b30f4da9e644a421f24b77ba
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-10-30 17:35:21 +0100

    it’s openssl.version

--- lib/opensslLegacyProvider.nix
+++ lib/opensslLegacyProvider.nix
@@ -2,6 +2,6 @@
   openssl,
   nodejs,
 }:
-if builtins.compareVersions nodejs.version "18" >= 0 && builtins.compareVersions openssl.versions "3" >= 0
+if builtins.compareVersions nodejs.version "18" >= 0 && builtins.compareVersions openssl.version "3" >= 0
 then "--openssl-legacy-provider"
 else ""
