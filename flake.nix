84ae35d2d369afd344314f2f75f0c52b8dd19f2d
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-03-24 08:42:45 +0100

    add rust-overlay flake input

--- flake.nix
+++ flake.nix
@@ -2,19 +2,30 @@
   description = "Lotte’s nix packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:DarkKirb/flake-utils";
     attic = {
       url = "github:zhaofengli/attic";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+    cargo2nix = {
+      url = "github:DarkKirb/cargo2nix/release-0.11.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+    flake-utils.url = "github:DarkKirb/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
     mautrix-cleanup = {
       url = "github:DarkKirb/mautrix-cleanup";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
