e6d83d8135f6a9ee9ebb6eb4fadcc72394502f51
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-10-18 06:42:47 +0100

    cleanup ubuntu container

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 
 ./scripts/update-git.sh https://github.com/starfive-tech/linux linux/vf2/source.json "--rev refs/heads/JH7110_VisionFive2_upstream"
 ./scripts/update-git.sh https://github.com/koverstreet/bcachefs linux/bcachefs/source.json
+./web/wordpress-plugins/update.sh
+./web/wordpress-themes/update.sh
 nix-shell ./scripts/update.nix
