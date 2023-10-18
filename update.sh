a995da897d6131e0d884d7df0ca739020c2cda4a
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-10-18 07:58:31 +0100

    disable linux kernel autoupdating

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./scripts/update-git.sh https://github.com/starfive-tech/linux linux/vf2/source.json "--rev refs/heads/JH7110_VisionFive2_upstream"
-./scripts/update-git.sh https://github.com/koverstreet/bcachefs linux/bcachefs/source.json
+#./scripts/update-git.sh https://github.com/starfive-tech/linux linux/vf2/source.json "--rev refs/heads/JH7110_VisionFive2_upstream"
+#./scripts/update-git.sh https://github.com/koverstreet/bcachefs linux/bcachefs/source.json
 ./web/wordpress-plugins/update.sh
 ./web/wordpress-themes/update.sh
 nix-shell ./scripts/update.nix
