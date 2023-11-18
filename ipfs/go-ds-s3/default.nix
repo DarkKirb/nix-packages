2844b66aa0bf9e8e2efd6a21e5ae3c7a8e4e83e1
GitHub <noreply@github.com>
2023-11-18 20:25:52 +0000

    Automatic Update

--- ipfs/go-ds-s3/default.nix
+++ ipfs/go-ds-s3/default.nix
@@ -6,8 +6,9 @@
   openssl,
   pkg-config,
   writeScript,
-  go_1_21
-}: buildGoApplication rec {
+  go_1_21,
+}:
+buildGoApplication rec {
   pname = "go-ds-s3";
   version = "0.24.0-rc2";
   src = fetchFromGitHub {
@@ -29,10 +30,10 @@
     chmod +x $out/go-ds-s3-plugin
   '';
   postConfigure = ''
-      chmod -R +w vendor/
-      for f in $(find vendor/github.com/ipfs/boxo/gateway/assets -type l) vendor/github.com/ipfs/go-graphsync/message/ipldbind/*.ipldsch; do
-          cp -v --remove-destination -f `readlink $f` $f
-      done
-    '';
+    chmod -R +w vendor/
+    for f in $(find vendor/github.com/ipfs/boxo/gateway/assets -type l) vendor/github.com/ipfs/go-graphsync/message/ipldbind/*.ipldsch; do
+        cp -v --remove-destination -f `readlink $f` $f
+    done
+  '';
   go = go_1_21;
 }
