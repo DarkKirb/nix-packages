d723fa92a8630077ccbea79ce1b387dbbd61601b
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-11-23 09:01:38 +0100

    fix matrix-media-repo build

--- matrix/matrix-media-repo/default.nix
+++ matrix/matrix-media-repo/default.nix
@@ -45,7 +45,7 @@ in
     ];
     CGO_ENABLED = "1";
     buildPhase = ''
-      GOBIN=$PWD/bin go install -v ./cmd/compile_assets
+      GOBIN=$PWD/bin go install -v ./cmd/utilities/compile_assets
       $PWD/bin/compile_assets
       GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.GitCommit=$(git rev-list -1 HEAD) -X github.com/turt2live/matrix-media-repo/common/version.Version=${version}" -v ./cmd/...
     '';
