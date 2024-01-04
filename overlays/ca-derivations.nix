c352122a66a268b7f5ecc0c4352854feb2372f03
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-01-04 18:02:39 +0100

    remove openldap testsuite

--- overlays/ca-derivations.nix
+++ overlays/ca-derivations.nix
@@ -2,4 +2,7 @@ self: super: {
     redis = super.redis.overrideAttrs (_: {
         doCheck = false;
     });
+    openldap = super.openldap.overrideAttrs (_: {
+        doCheck = false;
+    });
 }
